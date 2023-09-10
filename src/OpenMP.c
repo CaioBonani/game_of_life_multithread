ffe12b839a56138544fb3376e4161dc460bda22d
DaviSeiji <daviseiji@gmail.com>
2023-09-10 16:37:33 -0300

    inicial

--- src/OpenMP.c
+++ src/OpenMP.c
@@ -1,5 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
+#define N 2048
+#define nThreads 2
+#define nGenerations 2000
+
+void alocarMatriz(float ***matriz) {
+    *matriz = (float**)malloc(N * sizeof(float*));
+    for (int i = 0; i < N; i++) {
+        (*matriz)[i] = (float*)malloc(N * sizeof(float));
+    }
+}
+
+void desalocarMatriz(float ***matriz) {
+    for (int i = 0; i < N; i++) {
+        free((*matriz)[i]);
+    }
+    free(*matriz);
+}
+
+void zerarMatriz(float ***matriz) {
+    unsigned char* temp = (unsigned char*)malloc(N * N * sizeof(float));
+    memset(temp, 0, N * N * sizeof(float));
+    memcpy(*matriz, temp, N * N * sizeof(float));
+    free(temp);
+}
+
+void glider(float ***matriz) {
+    int lin = 1, col = 1;
+    (*matriz)[lin][col + 1] = 1.0;
+    (*matriz)[lin + 1][col + 2] = 1.0;
+    (*matriz)[lin + 2][col] = 1.0;
+    (*matriz)[lin + 2][col + 1] = 1.0;
+    (*matriz)[lin + 2][col + 2] = 1.0;
+}
+
+void rPentomino(float ***matriz) {
+    int lin = 10;
+    int col = 30;
+    (*matriz)[lin][col + 1] = 1.0;
+    (*matriz)[lin][col + 2] = 1.0;
+    (*matriz)[lin + 1][col] = 1.0;
+    (*matriz)[lin + 1][col + 1] = 1.0;
+    (*matriz)[lin + 2][col + 1] = 1.0;
+}
+
+void printarMatriz(float **matriz){
+
+    int i, j;
+
+    for(i=0; i < 50; i++){
+        for(j = 0; j < 50; j++){
+            
+            printf("[%2.f]", matriz[i][j]);
+        }
+    }
+}
+
+int main() {
+    float **grid, **newGrid;
+
+    alocarMatriz(&grid);
+    alocarMatriz(&newGrid);
+
+    zerarMatriz(&grid);
+    zerarMatriz(&newGrid);
+
+    glider(&grid);
+    rPentomino(&grid);
+
+    printf("\nTESTE");
+
+    printarMatriz(grid);
+
+    desalocarMatriz(&grid);
+    desalocarMatriz(&newGrid);
+
+    return 0;
 }
